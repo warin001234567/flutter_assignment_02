4c0dd476738339056f59dbb78ea881859ebf6ef4
warin001234567 <60070089@kmitl.ac.th>
2019-04-07 20:35:38 +0700

    second

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,12 +1,12 @@
 import 'package:sqflite/sqflite.dart';
 
 final String tableTodo = "todo";
-final String columnId = "_id";
+final String columnId = "id";
 final String columnTitle = "title";
 final String columnDone = "done";
 
 class Todo {
-  int _id;
+  int id;
   String title;
   bool done = false;
 
@@ -15,8 +15,8 @@ Map<String, dynamic> toMap(){
     columnTitle: title,
     columnDone: done == true ? 1 : 0
   };
-  if (_id != null){
-    map[columnId] = _id;
+  if (id != null){
+    map[columnId] = id;
   }
   return map;
 }
@@ -26,7 +26,7 @@ Map<String, dynamic> toMap(){
   }
 
   Todo.fromMap(Map<String, dynamic> map){
-    _id = map[columnId];
+    id = map[columnId];
     title = map[columnTitle];
     done = map[columnDone] == 1;
   }
@@ -53,7 +53,7 @@ class TodoProvider {
   }
 
   Future<Todo> insert(Todo todo) async{
-    todo._id = await db.insert(tableTodo, todo.toMap());
+    todo.id = await db.insert(tableTodo, todo.toMap());
     return todo;
   }
 
@@ -72,7 +72,7 @@ class TodoProvider {
       tableTodo,
       todo.toMap(),
       where: '$columnId = ?',
-      whereArgs: [todo._id]
+      whereArgs: [todo.id]
       );
   }
 
